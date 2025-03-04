6471a2688daf301afc520e56153dd53965a68c39
mjunnn <mingjun1132@gmail.com>
2025-03-04 22:58:52 +0800

    try

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # video-call-backend
+
 1. The Program.cs already setup for JWT and Google Authentication
 2. The appsetting,json file have my client id and secret key.
 3. The auth controller already have login from front end (if not sure dont modify it first XD)
 4. User controller will need to have get and post function for get user data and post for posting the picture to backend and the backend will also return user data.
+
+halo
--- VideoActive/obj/Debug/net9.0/VideoActive.AssemblyInfo.cs
+++ VideoActive/obj/Debug/net9.0/VideoActive.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VideoActive")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c332aa3f1e8048248564210fdfe5a18534e5bff7")]
 [assembly: System.Reflection.AssemblyProductAttribute("VideoActive")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VideoActive")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
